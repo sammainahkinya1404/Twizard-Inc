50b2c694d1fc7646a4fe9d2bf202fd0efdde30f2
Samson Kinyanjui <sammainah98@gmail.com>
2023-08-24 22:43:17 +0300

    Django templating language

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('shop.urls')),
 ]
 if settings.DEBUG:
     urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product,Category
 
 # Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    return render(request,'base.html',{'products':products}) 
+def home(request):
+    
+   
+    return render(request,'home.html')
